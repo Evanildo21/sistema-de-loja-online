7d4ea706db3f4d86a78feaeabbde3c789144a073
evanildo <evanildotavares78@gmail.com>
2022-11-02 19:27:47 -0300

    primeiro commit

---
+++ trabalho de Poo/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "figura.hpp"
+using namespace std;
+
+
+
+int main()
+{
+ola();
+
+    return 0;
+}
---
+++ trabalho de Poo/ususario.hpp
@@ -0,0 +1,10 @@
+#ifndef FIGURA_H_INCLUDE
+#define FIGURA_H_INCLUDE
+#include <iostream>
+using namespace std;
+
+void ola();
+int soma(int a,int b);
+
+
+#endif
